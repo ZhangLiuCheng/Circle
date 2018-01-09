5a366470ae564a1efc22bb996743ad77253ae188
zhangliucheng <414616504@qq.com>
2018-01-09 10:23:26 +0800

    添加我的里面测试布局

--- pages/my/my.js
+++ pages/my/my.js
@@ -14,7 +14,24 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    list: [
+      {
+        id: 'view',
+        name: '视图容器',
+        open: false,
+        pages: ['view', 'scroll-view', 'swiper']
+      }, {
+        id: 'content',
+        name: '基础内容',
+        open: false,
+        pages: ['text', 'icon', 'progress']
+      }, {
+        id: 'form',
+        name: '表单组件',
+        open: false,
+        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch', 'textarea']
+      }
+    ]
   },
 
   /**
