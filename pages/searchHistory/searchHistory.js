f5cb2691c2057d61040850f8f00d517855d75520
zhangliucheng <414616504@qq.com>
2018-01-30 17:22:08 +0800

    修改搜索界面ui

--- pages/searchHistory/searchHistory.js
+++ pages/searchHistory/searchHistory.js
@@ -7,10 +7,49 @@ Component({
 
   data: {
     open: false,
+    searchEnable: false,
+    keyword: ''
   },
 
   methods: {
-    swichNav: function (e) {
+
+    searchTextInput: function(e) {
+      // console.log(e.detail.value.length)
+      this.setData({
+        keyword: e.detail.value
+      })
+      this.searchBtnState()
+    },
+
+    searchTextFocus:function (e) {
+      this.showPanel();
+    },
+
+    // 输入框点击键盘搜索按钮
+    searchTextConfirm: function (e) {
+      this.searching()
+    },
+
+    searching: function () {
+      console.log('searching')
+      this.setData({
+        keyword:''
+      })
+      this.searchBtnState()
+      this.closePanel()
+    },
+
+    // 设置<搜索>按钮是否可点击状态
+    searchBtnState: function() {
+      var enable = this.data.keyword.length > 0
+      if (enable != this.data.searchEnable) {
+        this.setData({
+          searchEnable: enable
+        })
+      }
+    },
+
+    showPanel: function () {
       this.setData({
         open: true
       })
